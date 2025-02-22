852c7305053e5cd183cc693f8dda1da5e0b87e25
Enotjao <enotjao@gmail.com>
2025-02-23 01:28:29 +0200

    Delete TetHomeTask directory

--- TetHomeTask/TetHomeTask/FlagsApi.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-class FlagsApi {
-    static let baseURL = "https://flagsapi.com";
-
-    class func getCountryFlagImageUrl(countryCode: string) -> String {
-        return "\(baseURL)/\(countryCode)/flat/64.png";
-    }
-}
